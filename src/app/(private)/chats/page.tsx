0b57b228294efd3776a3ee90792e2b44b7884e6c
Saulo Costa <saulotarsobc@gmail.com>
2025-04-07 20:35:01 -0300

    mock data chats

--- src/app/(private)/chats/page.tsx
+++ src/app/(private)/chats/page.tsx
@@ -1,3 +1,4 @@
+import { MockChats } from "@/app/mock/MockChats";
 import ContentContainer from "@/components/ContentContainer";
 import Side from "@/components/Side";
 import style from "./page.module.css";
@@ -5,8 +6,22 @@ import style from "./page.module.css";
 const Page = () => {
   return (
     <main className={style.page}>
-      <Side>Side</Side>
-      <ContentContainer>Listagem de chats</ContentContainer>
+      <Side>
+        <div style={{ height: "50px" }}>Header</div>
+        <div>
+          {MockChats.map((chat) => (
+            <div key={chat.id}>
+              <p>{chat.created_at}</p>
+              <p>{chat.name}</p>
+              <p>{chat.message}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      </Side>
+      <ContentContainer>
+        <p>conteudo do chat</p>
+      </ContentContainer>
     </main>
   );
 };
