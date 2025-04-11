62b4203b7e15faed037fe77911e0a9feaea87ae6
Saulo Costa <saulotarsobc@gmail.com>
2025-04-11 07:13:42 -0300

    feat(home): add styled login button with navigation
    Introduce a styled login button on the home page that redirects to the chats page when clicked. The button is styled using a new CSS module to improve the visual appearance and user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import style from './style.module.css';
+
 export default function Home() {
-  return <main>Login</main>;
+  return (
+    <main className={style.Home}>
+      <button className={style.Button} onClick={() => window.location.href = '/chats'}>Logar</button>
+    </main>
+  );
 }
